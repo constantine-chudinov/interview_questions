7e2a86ceef5da1d3e2c2cd639cb1919a71b07c7b
Konstantin Chudinov <kchudinov@smartling.com>
2021-02-12 11:20:10 +0300

    Added package-lock and one more test

--- sum_arrays/index.test.js
+++ sum_arrays/index.test.js
@@ -20,6 +20,11 @@ test("With negative numbers but sum is positive", () => {
     expect(checkArrayForSum([100, -33, 110, 44], 11)).toBe(true);
 });
 
+
+test("Should not sum each element twice", () => {
+    expect(checkArrayForSum([4, 7, 1, 3], 14)).toBe(false);
+});
+
 test("With negative numbers but sum is negative", () => {
     expect(checkArrayForSum([100, -33, 110, 22], -11)).toBe(true);
 });
